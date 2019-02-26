8cbbdb5d56e564138ab511b91302f5a03a93c17f
Ryland Goldstein <ryland@binaris.com>
2019-02-26 11:14:27 -0800

    add slack-pi-command tutorial/example

---
+++ slack-pi-command/function.js
@@ -0,0 +1,25 @@
+const { parse } = require('query-string');
+const PI = require('pi');
+
+exports.handler = async (body, context) => {
+  // slack unfortunately sends command payloads as x-url-form-encoded
+  const parsed = parse(context.body.toString('utf8'));
+
+  // ensure the input is a valid, real number
+  const numDigits = parseInt(parsed.text, 10);
+  if (!numDigits || isNaN(numDigits)) {
+    return {
+      text: `Expected integer param, received ${typeof(numDigits)}`,
+      response_type: 'ephemeral',
+    };
+  }
+
+  // enforce a fun limit on the max pi digits
+  if (numDigits > 10) {
+    return {
+      text: "You've already had too much 🥧!",
+      response_type: 'ephemeral',
+    };
+  }
+  return PI(numDigits - 1);
+};
