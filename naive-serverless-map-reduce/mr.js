74aafa0813380d7c68f85157f6aeced525c4c225
Avner Braverman <avner@binaris.com>
2019-03-27 12:29:47 -0700

    Apply Ryland's comments

--- naive-serverless-map-reduce/mr.js
+++ naive-serverless-map-reduce/mr.js
@@ -1,51 +1,15 @@
-const request = (() => {
-  const https = require('https');
-  const agent = new https.Agent({ keepAlive: true });
-  return (method, host, path, headers, body) => {
-    return new Promise((resolve, reject) => {
-      const opts = { method, host, path, headers, agent, port: 443 };
-      const req = https.request(opts, res => {
-        const chunks = [];
-        res.on('data', chunk => chunks.push(chunk));
-        res.on('end', () => (res.statusCode === 200 ? resolve : reject)({
-          status: res.statusCode,
-          headers: res.headers,
-          body: chunks.join('')
-        }));
-      });
-      req.on('error', e => reject({
-       status: -1,
-        headers: {},
-        body: `Network error: ${e}`
-      }));
-      req.write(body);
-      req.end();
-    });
-  };
-})();
+const request = require('request-promise-native');
 
 const invoke = async (fname, fargs) => {
   console.log(`Invoking ${fname}()`);
-  if (typeof(fname) !== 'string' || !/^[a-zA-Z_][a-zA-Z0-9_]*$/.test(fname)) {
-    throw new Error(`Invalid function name: ${fname}`);
-  }
-  const headers = { 'content-type': 'application/json' };
-  if (!fname.startsWith('public_')) {
-    if (!process.env.BINARIS_ACCOUNT_ID) {
-      throw new Error(`Missing API key for invoking protected function: ${fname}`);
-    }
-    headers['X-Binaris-Api-Key'] = process.env.BINARIS_ACCOUNT_ID;
-  }
-  const response = await request('post',
-                                 'run.binaris.com',
-                                 `/v2/run/${process.env.BINARIS_ACCOUNT_ID}/${fname}`,
-                                 headers,
-                                 JSON.stringify(fargs));
-  console.log(`${fname}() -> ${response.body}`);
-  if (!response.headers['content-type'].startsWith('application/json')) {
-    throw new Error(`Invalid response type: ${response.headers['content-type']}`);
-  }
-  return response.body.length ? JSON.parse(response.body) : undefined;
+  const uri = `https://run.binaris.com/v2/run/${process.env.BINARIS_ACCOUNT_ID}/${fname}`;
+  const res = await request.post(uri, {
+    headers: { 'content-type': 'application/json' },
+    json: true,
+    body: fargs
+  });
+  console.log(`${fname}() -> ${res}`);
+  return res;
 };
 
 exports.controller = async job => {
