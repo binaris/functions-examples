1adb65d25186c9d1d1deb9a7b30cccb03c8e7eaf
Avner Braverman <avner@binaris.com>
2019-04-25 16:07:31 -0700

    apply rylands comments

--- image-resize/function.js
+++ image-resize/function.js
@@ -1,13 +1,20 @@
-const cp = require('child_process');
+const { exec } = require('child_process');
+const { readFile, writeFile } = require('fs');
+const { promisify } = require('util');
 const request = require('request-promise-native');
-const file = require('./file');
+
+const asyncExec = promisify(exec);
+const asyncReadFile = promisify(readFile);
+const asyncWriteFile = promisify(writeFile);
+
+const SRCFILE = '/tmp/srcimg';
+const DSTFILE = '/tmp/dstimg.png';
 
 exports.handler = async (body, context) => {
-
   // Get an image URL from the request body or query
   // string (useful for invoking directly from a browser)
   const uri = body.url || context.request.query.url;
-  if (!uri) {
+  if (uri === undefined || uri === '') {
     // This will be caught by Binaris and returned as
     // a HTTP 500 status code
   	throw new Error('No URL');
@@ -17,26 +24,23 @@ exports.handler = async (body, context) => {
   // resized to {size} by {size} pixels. If neither
   // request body nor query string contains a size
   // value then default to 32
-  const size = parseInt(body.sz) || parseInt(context.request.query.sz) || 32;
+  const size = parseInt(body.sz, 10) ||
+               parseInt(context.request.query.sz, 10) ||
+               32;
 
   // Load the image from the specified url and save
   // to file in /tmp
   console.log(`Loading: ${uri}`);
-  const srcimg = await request({
-	  uri,
-	  method: 'GET',
-	  encoding: null,
-  });
+  const srcImage = await request({ uri, encoding: null });
   console.log(`Image has ${srcimg.length} bytes`);
-  await file.write('/tmp/srcimg', srcimg);
+  await asyncWriteFile(SRCFILE, srcImage);
 
   // Use ImageMagick to resize and convert to PNG.
   // Always returning a PNG simplifies the response
   // encoding code below
-  let out;
   try {
     console.log(`Resiging to ${size}x${size} pixels`);
-    cp.execSync(`convert /tmp/srcimg -resize ${size}x${size} /tmp/dstimg.png`);
+    await asyncExec(`convert ${SRCFILE} -resize ${size}x${size} ${DSTFILE}`);
   } catch (e) {
     console.error('Error resizing image:');
     console.error(e.stderr.toString());
@@ -45,7 +49,7 @@ exports.handler = async (body, context) => {
   }
 
   // Read the resized image file
-  const dstimg = await file.read('/tmp/dstimg.png');
+  const dstImage = await asyncReadFile(DSTFILE);
 
   // Return an HTTP response with PNG encoded binary
   // image
@@ -54,6 +58,6 @@ exports.handler = async (body, context) => {
     headers: {
       'Content-Type': 'image/png',
     },
-    body: dstimg,
+    body: dstImage,
   });
 };
